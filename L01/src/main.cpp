ec49de0f6ab556d34e8f314acc5a5a862649dea2
Rhianna So <rhiannaso@umail.ucsb.edu>
2021-03-31 22:12:45 -0700

    Partial triangle

--- L01/src/main.cpp
+++ L01/src/main.cpp
@@ -25,6 +25,11 @@ struct BoundingBox {
    int ymax;
 };
 
+// Triangle
+struct Triangle t;
+// Bounding Box
+struct BoundingBox bb;
+
 void setTriangle(struct Triangle *t, int ax, int ay, int bx, int by, int cx, int cy) {
    t->x1 = ax;
    t->y1 = ay;
@@ -72,10 +77,62 @@ void calcBounds( struct Triangle t, struct BoundingBox *bb ) {
     setBoundingBox(bb, xmin, xmax, ymin, ymax);
 }
 
+bool inTriangle(float val) {
+    return ((val > 0) && (val < 1));
+}
+
+bool isEdge(float alpha, float beta, float gamma) {
+    int isZero = 0;
+    int isWithin = 0;
+
+    float arr[3] = {alpha, beta, gamma};
+
+    for(int i=0; i < 3; i++) {
+        if(arr[i] == 0) {
+            isZero++;
+        }
+        if(inTriangle(arr[i])) {
+            isWithin++;
+        }
+    }
+
+    if(isZero == 1 && isWithin == 2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void colorPixel(int x, int y, float alpha, float beta, float gamma, shared_ptr<Image> image) {
+    // If in triangle
+    if(inTriangle(alpha) && inTriangle(beta) && inTriangle(gamma)) {
+        cout << "HERE\n";
+        image->setPixel(x, y, 255, 255, 255);
+    }
+    if (isEdge(alpha, beta, gamma)) { // If is triangle's edge
+        cout << "THERE\n";
+        image->setPixel(x, y, 220, 220, 220);
+    }
+}
+
+void calcCoords(int oldX, int oldY, shared_ptr<Image> image) {
+    int ycya = t.y3-t.y1;
+    int ybya = t.y2-t.y1;
+    int xcxa = t.x3-t.x1;
+    int xbxa = t.x2-t.x1;
+    float divisor = 1 - ((ycya*xbxa)/(ybya*xcxa));
+    float sub1 = (oldY-t.y1)/ybya;
+    float sub2 = (ycya*(oldX-t.x1))/(ybya*xcxa);
+    float beta = (sub1-sub2)/divisor;
+    float gamma = (oldX - t.x1 - (beta*xbxa))/xcxa;
+    float alpha = 1 - beta - gamma;
+    colorPixel(oldX, oldY, alpha, beta, gamma, image);
+}
+
 int main(int argc, char **argv)
 {
 	if(argc < 4) {
-		cout << "Usage: Lab1 <out_image_name>.png width height vax vay vbx vby vcx vcy" << endl;
+		cout << "Usage: Lab1 <out_image_name>.png width height x1 y1 r1 g1 b1 x2 y2 r2 g2 b2 x3 y3 r3 g3 b3" << endl;
 		return 0;
 	}
 	// Output filename
@@ -88,21 +145,35 @@ int main(int argc, char **argv)
     int x1 = atoi(argv[4]);
     // Vertex 1 y-coordinate
     int y1 = atoi(argv[5]);
+    // Vertex 1 r value
+    int r1 = atoi(argv[6]);
+    // Vertex 1 g value
+    int g1 = atoi(argv[7]);
+    // Vertex 1 b value
+    int b1 = atoi(argv[8]);
     // Vertex 2 x-coordinate
-    int x2 = atoi(argv[6]);
+    int x2 = atoi(argv[9]);
     // Vertex 2 y-coordinate
-    int y2 = atoi(argv[7]);
+    int y2 = atoi(argv[10]);
+    // Vertex 2 r value
+    int r2 = atoi(argv[11]);
+    // Vertex 2 g value
+    int g2 = atoi(argv[12]);
+    // Vertex 2 b value
+    int b2 = atoi(argv[13]);
     // Vertex 3 x-coordinate
-    int x3 = atoi(argv[8]);
+    int x3 = atoi(argv[14]);
     // Vertex 3 y-coordinate
-    int y3 = atoi(argv[9]);
+    int y3 = atoi(argv[15]);
+    // Vertex 3 r value
+    int r3 = atoi(argv[16]);
+    // Vertex 3 g value
+    int g3 = atoi(argv[17]);
+    // Vertex 3 b value
+    int b3 = atoi(argv[18]);
 
-    // Triangle
-    struct Triangle t;
     setTriangle(&t, x1, y1, x2, y2, x3, y3);
     //printT(t);
-    // Bounding Box
-    struct BoundingBox bb;
     calcBounds(t, &bb);
 
 	// Create the image. We're using a `shared_ptr`, a C++11 feature.
@@ -111,26 +182,27 @@ int main(int argc, char **argv)
 	// Draw bounding box
 	for(int y = bb.ymin; y < bb.ymax+1; ++y) {
 		for(int x = bb.xmin; x < bb.xmax+1; ++x) {
-            unsigned char r;
-            unsigned char g;
-            if (x % 2 == 0) {
-                r = 255;
-                g = 0;
-            } else {
-                r = 0;
-                g = 255;
-            }
-			// unsigned char r = 255;
-			// unsigned char g = 0;
-			unsigned char b = 0;
-			image->setPixel(x, y, r, g, b);
+            calcCoords(x, y, image);
+            // unsigned char r;
+            // unsigned char g;
+            // if (x % 2 == 0) {
+            //     r = 0;
+            //     g = 0;
+            // } else {
+            //     r = 0;
+            //     g = 0;
+            // }
+			// // unsigned char r = 255;
+			// // unsigned char g = 0;
+			// unsigned char b = 0;
+			// image->setPixel(x, y, r, g, b);
 		}
 	}
 
     // Draw vertices
-    image->setPixel(x1, y1, 255, 255, 255);
-    image->setPixel(x2, y2, 255, 255, 255);
-    image->setPixel(x3, y3, 255, 255, 255);
+    image->setPixel(x1, y1, r1, g1, b1);
+    image->setPixel(x2, y2, r2, g2, b2);
+    image->setPixel(x3, y3, r3, g3, b3);
 
 	// Write image to file
 	image->writeToFile(filename);
